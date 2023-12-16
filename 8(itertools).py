79b34c7b991d9580980cfa3af06b1403de9de58d
Alexeyuey <110528832+Alexeyuey@users.noreply.github.com>
2023-12-16 11:25:59 +0400

    Update 8(itertools).py

--- 8(itertools).py
+++ 8(itertools).py
@@ -1,5 +1,5 @@
 from itertools import *
 
-w = Ip for p in permutations ('КАПКАН")
+w = (p for p in permutations ('КАПКАН"))
 if ''.join(p).count('AA')+''.join(p).count('КК'):
-print(len(w)) 
+    print(len(w))  
